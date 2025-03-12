7e5a750da0eb87fe2a76ed31efa71697b3294051
nebulis33 <>
2025-03-11 17:10:22 -0700

    Read JSON data into the relevant tables

---
+++ read_data.py
@@ -0,0 +1,149 @@
+import duckdb
+
+
+def create_users(connection):
+    connection.execute(
+        """
+        CREATE OR REPLACE TABLE users (
+            id TEXT PRIMARY KEY,
+            active BOOLEAN,
+            created_date TIMESTAMP,
+            last_login TIMESTAMP,
+            role TEXT,
+            sign_up_source TEXT,
+            state TEXT
+        );
+        """
+    )
+
+    connection.execute(
+        """
+        BEGIN TRANSACTION;
+        INSERT OR IGNORE INTO users
+        SELECT
+            "_id"."$oid" AS id,
+            active, 
+            to_timestamp("createdDate"."$date"/1000) AS created_date, 
+            to_timestamp("lastLogin"."$date"/1000) AS last_login, 
+            role, 
+            signUpSource AS sign_up_source, 
+            state
+        FROM read_json('raw_data/users.json');
+        COMMIT;
+        """)
+
+
+def create_brands(connection):
+    connection.execute(
+        """
+        CREATE OR REPLACE TABLE brands (
+            id TEXT PRIMARY KEY,
+            name TEXT,
+            barcode TEXT,
+            brand_code TEXT,
+            category TEXT,
+            category_code TEXT,
+            cpg_id TEXT,
+            top_brand BOOLEAN
+            );
+        """
+    )
+
+    connection.execute(
+        """
+        BEGIN TRANSACTION;
+        INSERT OR IGNORE INTO brands
+        SELECT
+            "_id"."$oid" AS id,
+            name,
+            barcode,
+            brandCode AS brand_code,
+            category,
+            categoryCode AS category_code,
+            "cpg"."$id"."$oid" AS cpg_id,
+            topBrand AS top_brand
+        FROM read_json('raw_data/brands.json');
+        COMMIT;
+        """
+    )
+
+
+def create_receipts(connection):
+    connection.execute(
+        """
+        CREATE OR REPLACE TABLE receipts (
+            id TEXT PRIMARY KEY,
+            bonus_points_earned INT,
+            bonus_points_reason TEXT,
+            creation_date TIMESTAMP,
+            scan_date TIMESTAMP,
+            finish_date TIMESTAMP,
+            modified_date TIMESTAMP,
+            points_awarded_date TIMESTAMP,
+            points_earned INT,
+            purchase_date TIMESTAMP,
+            purchased_item_count INT,
+            status TEXT,
+            total_spent FLOAT,
+            user_id TEXT,
+            reward_list TEXT
+            );
+        """
+    )
+
+    connection.execute(
+        """
+        BEGIN TRANSACTION;
+        INSERT OR IGNORE INTO receipts
+        SELECT
+            "_id"."$oid" AS id,
+            bonusPointsEarned AS bonus_points_earned,
+            bonusPointsEarnedReason AS bonus_points_reason,
+            to_timestamp("createDate"."$date" / 1000) AS creation_date,
+            to_timestamp("dateScanned"."$date" / 1000) AS scan_date,
+            to_timestamp("finishedDate"."$date" / 1000) AS finish_date,
+            to_timestamp("modifyDate"."$date" / 1000) AS modified_date,
+            to_timestamp("pointsAwardedDate"."$date" / 1000) AS points_awarded_date,
+            pointsEarned AS points_earned,
+            to_timestamp("purchaseDate"."$date" / 1000) AS purchase_date,
+            purchasedItemCount AS purchase_item_count,
+            rewardsReceiptStatus AS status,
+            totalSpent AS total_spent,
+            userID AS user_id,
+            rewardsReceiptItemList AS reward_list
+        FROM read_json('raw_data/receipts.json');
+        COMMIT;
+        """
+    )
+
+
+def create_purchases(connection):
+    connection.execute(
+        """
+        CREATE OR REPLACE TABLE purchases (
+            id TEXT PRIMARY KEY,
+            receipt_id TEXT,
+            barcode TEXT,
+            description TEXT,
+            brand_id TEXT,
+            item_price FLOAT,
+            discounted_price FLOAT,
+            final_price FLOAT,
+            purchase_quantity INT,
+            needs_fetch_review BOOLEAN,
+            prevent_target_gap_points BOOLEAN,
+            user_flagged_barcode TEXT,
+            user_flagged_new_item BOOLEAN,
+            user_flagged_price FLOAT,
+            user_flagged_purchase_quantity INT
+            );
+        """
+    )
+
+if __name__ == "__main__":
+    db = duckdb.connect(database="takehome-db.duckdb", read_only = False)
+    create_users(db)
+    create_brands(db)
+    create_receipts(db)
+    create_purchases(db)
+    db.close()
